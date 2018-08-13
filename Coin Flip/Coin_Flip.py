2636cff6fe490fd9369aa1a8e2a0607f051d68af
Matthew Lake <matt@matthewlake.co.uk>
2018-08-13 16:26:18 +0100

    initial

--- Coin Flip/Coin_Flip.py
+++ Coin Flip/Coin_Flip.py
@@ -1,1 +1,27 @@
+import random
 
+play = input('Press T to toss a coin')
+
+heads = 0
+tails = 0
+
+def toss_coin():
+    toss = random.randint(1,2)
+    if(toss == 1):
+        global heads
+        heads += 1
+        print('It was Heads')
+    else:
+        global tails
+        tails += 1
+        print("It was tails")
+
+
+while play == 't':
+    toss_coin()
+    print('-------------------------------')
+    print('|  Heads:  |  '+str(heads)+'  |')
+    print('-------------------------------')
+    print('|  Tails:  |  '+str(tails)+'  |')
+    print('-------------------------------')
+    play = input('Press T to toss a coin')
